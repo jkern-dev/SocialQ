7f1018b8edcf790d2aa916bf0a76b03e6031c8b1
Josh Kern <jkern.dev@gmail.com>
2019-05-12 13:28:55 -0700

    user auth frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
+import NavBarContainer from './navbar/navbar_container';
+
 import SignUpFormContainer from './session/signup_form_container';
+import LoginFormContainer from './session/login_form_container';
 
 import { Route } from 'react-router';
 
 const App = () => (
   <div>
-    <Route exact path = "/signup" component = {SignUpFormContainer} />
+    <NavBarContainer />
+    <Switch>
+      <Route exact path = "/signup" component = {SignUpFormContainer} />
+      <Route exact path="/login" component={LoginFormContainer} />
+    </Switch>
   </div>
 )
 
---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.getLinks = this.getLinks.bind(this);
+  }
+
+  logoutUser(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+  getLinks() {
+    if (this.props.loggedIn) {
+      return (
+        <div>
+          <h1>Hello, {this.props.currentUser.username}</h1>
+          <button onClick = {this.logoutUser}>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to={'/signup'}>Sign Up</Link>
+          <Link to={'/login'}>Log In</Link>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Social Q's</h1>
+        { this.getLinks() }
+      </div>
+    )
+  }
+}
+
+export default NavBar;
