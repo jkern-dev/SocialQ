710037469068b4616cecf44f0f8fe0d56300a281
Ajay <ajayvishwanath.94@gmail.com>
2019-05-13 11:05:48 -0700

    Begin Styling

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -17,28 +17,30 @@ class NavBar extends React.Component {
   getLinks() {
     if (this.props.loggedIn) {
       return (
-        <div>
-          <h1>Hello, {this.props.currentUser.username}</h1>
-          <button onClick = {this.logoutUser}>Logout</button>
-          <Link to={'/new_question'}>Submit A Question</Link>
-        </div>
+          <div>
+            <h1>Hello, {this.props.currentUser.username}</h1>
+            <button onClick = {this.logoutUser}>Logout</button>
+            <Link to={'/new_question'}>Submit A Question</Link>
+          </div>
       );
     } else {
       return (
-        <div>
-          <Link to={'/signup'}>Sign Up</Link>
-          <Link to={'/login'}>Log In</Link>
-        </div>
+          <div>
+            <Link to={'/signup'}>Sign Up</Link>
+            <Link to={'/login'}>Log In</Link>
+          </div>
       );
     }
   }
 
   render() {
     return (
-      <div>
-        <Link to="/"><h1>Social Q's</h1></Link>
-        { this.getLinks() }
-      </div>
+      <div className="navbar-container">
+        <div>
+          <Link to="/"><h1>Social Q's</h1></Link>
+          { this.getLinks() }
+        </div>
+      </div> 
     )
   }
 }
