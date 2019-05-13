0567d5b8966767bccfa0cc117a0b741e7a1751e9
Josh Kern <jkern.dev@gmail.com>
2019-05-13 11:58:41 -0700

    chart.js visualization for answer

--- frontend/src/reducers/questions_reducer.js
+++ frontend/src/reducers/questions_reducer.js
@@ -1,10 +1,11 @@
 import {
   RECEIVE_QUESTIONS,
   RECEIVE_USER_QUESTIONS,
-  RECEIVE_NEW_QUESTION
+  RECEIVE_NEW_QUESTION,
+  RECEIVE_QUESTION
 } from '../actions/question_actions';
 
-const QuestionReducer = (state = {all: {}, user: {}, new: undefined}, action) => {
+const QuestionReducer = (state = {all: {}, question: {}, user: {}, new: undefined}, action) => {
   Object.freeze(state);
   let newState = Object.assign({},state);
   switch(action.type) {
@@ -16,6 +17,9 @@ const QuestionReducer = (state = {all: {}, user: {}, new: undefined}, action) =>
       return newState;
     case RECEIVE_NEW_QUESTION:
       newState.new = action.question.data;
+      return newState;
+    case RECEIVE_QUESTION:
+      newState.question = action.question.data
       return newState;
     default:
       return state;
